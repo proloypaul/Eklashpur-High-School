65850c42c430adb64b2d7408ecadcb347426ad62
proloypaul <proloyp23@gmail.com>
2021-10-04 01:22:21 +0600

    web structure

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import About from '../About/About';
+import Classes from '../Classes/Classes';
+import Searvices from '../Services/Searvices';
 
 const Home = () => {
     return (
         <div className="border-4 border-black p-20">
             <h1>Home section</h1>
+            <About></About>
+            <Searvices></Searvices>
+            <Classes></Classes>
         </div>
     );
 };
