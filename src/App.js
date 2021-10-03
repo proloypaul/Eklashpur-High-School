dab1f446f4ca78392e798585db1266157c788d54
proloypaul <proloyp23@gmail.com>
2021-10-04 00:02:52 +0600

    add tailwind

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>WelCome to our school</h1>
+      <h1 className="text-6xl">WelCome to our school</h1>
+      
     </div>
   );
 }
