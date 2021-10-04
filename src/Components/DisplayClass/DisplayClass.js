973c1c913706c2f4678f65a41d112e76f02090a5
proloypaul <proloyp23@gmail.com>
2021-10-04 16:48:26 +0600

    clsses done

--- src/Components/DisplayClass/DisplayClass.js
+++ src/Components/DisplayClass/DisplayClass.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 const DisplayClass = (props) => {
     // console.log(props.singleClass)
-    const {teacher, img, subject, id} = props.singleClass
+    const {teacher, img, subject, time, day} = props.singleClass
     return (
         <div className="my-5">
             <div className="flex justify-between items-center border-2 border-gray-200 rounded w-full py-2 px-5">
@@ -11,9 +11,15 @@ const DisplayClass = (props) => {
                     <img src={img} alt="Empty!" width="100" height="100px" className="border-0 rounded-3xl"/>
                     <p className="text-xl font-bold">{teacher}</p>
                 </div>
-                <p className="text-xl font-semibold">Subject: {subject}</p>
+                <div>
+                    <p className="text-xl font-semibold">Subject: {subject}</p>
+                    <p className="text-sm font-light">Day: {day}</p>
+                    <p className="text-sm font-light">Time: {time}</p>
+
+                </div>
+                
                 <div className="text-center">
-                    <button className="bg-green-400 px-5 py-2 font-bold rounded"><Link to="/home">Class Details</Link></button>
+                    <button className="bg-green-400 px-5 py-2 font-bold rounded">Start Class</button>
                 </div>
             </div>
             
