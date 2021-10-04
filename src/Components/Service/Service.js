8dcc53d2e8325a1c5cad516d14480128116b1f66
proloypaul <proloyp23@gmail.com>
2021-10-04 12:40:33 +0600

    modify service

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 const Service = (props) => {
-    const {teacher, phone, img, subject, salary, education, position} = props.service
+    const {teacher, phone, img, salary, education, position} = props.service
     // console.log(props.service)
+    
     return (
         <div>
-            <div className="flex p-5 border-4 border-black">
-                <div>
-                    <img src={img} alt="Empty!"/>
+            <div className="flex justify-evenly items-center m-5 p-5 border-2 border-gray-200 rounded ">
+                <div className="w-1/4 h-1/4">
+                    <img src={img} alt="Empty!" className="border-0 rounded "/>
                 </div>
-                <div>
-                <p>Teacher Name: {teacher}</p>
-                <p>{position}</p>
-                <p>Education: {education}</p>
-                <p>Phone: {phone}</p>
+                <div className="text-xl font-semibold">
+                    <p className="m-5">Teacher Name: {teacher}</p>
+                    <p className="m-5">{position}</p>
+                    <p className="m-5">Salary: {salary}</p>
+                    <p className="m-5">Education: {education}</p>
+                    <p className="m-5">Phone: {phone}</p>
                 </div>
             </div>
             
